ee43ca04e5bfa2faca95fa573ce02a5c400fec13
Grzegorz Nęcka <strzypior@gmail.com>
2022-07-13 22:11:28 +0200

    new InferGetStaticPaths type

--- utils/types.ts
+++ utils/types.ts
@@ -1,3 +1,14 @@
 import { MDXRemoteSerializeResult } from "next-mdx-remote";
 
 export type MarkdownResult = MDXRemoteSerializeResult<Record<string, unknown>>;
+
+import type { GetStaticPropsContext } from "next";
+import type { ParsedUrlQuery } from "querystring";
+
+export type InferGetStaticPathsType<T> = T extends () => Promise<{
+    paths: Array<{ params: infer R }>;
+}>
+    ? R extends ParsedUrlQuery
+        ? GetStaticPropsContext<R>
+        : never
+    : never;
