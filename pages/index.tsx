58676c06bd4b6d6d3c58d448b27376e7454b442d
Grzegorz Nęcka <strzypior@gmail.com>
2022-07-20 23:41:32 +0200

    test graphql request with getStaticProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,13 +19,6 @@ const Home = () => {
     return (
         <Main>
             <pre>{JSON.stringify(data, null, 2)}</pre>
-
-            <h2 className="text-2xl font-extrabold tracking-tight text-gray-900 pb-5">sklaep - strona główna</h2>
-            <article>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur beatae explicabo eum. Omnis
-                quibusdam dolorem quis eos itaque corrupti repudiandae. Voluptate eum, pariatur corporis nobis nisi
-                aliquid at maxime id?
-            </article>
         </Main>
     );
 };
