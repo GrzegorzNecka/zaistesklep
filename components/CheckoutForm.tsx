1ddf765b5bbb7a2e1e73667ba923df9415cf90d8
GrzegorzNecka <global-strzypior@gmail.com>
2022-07-25 16:39:59 +0200

    add react-hook-form

--- components/CheckoutForm.tsx
+++ components/CheckoutForm.tsx
@@ -1,10 +1,46 @@
 //https://tailwindui.com/components/ecommerce/components/checkout-forms
 
+import { ChangeEventHandler, FormEventHandler, useState } from "react";
+
+interface CheckoutFormDAta {
+    firstName: string;
+    lastName: string;
+    emailAdress: string;
+    address: string;
+    city: string;
+    postalCode: string;
+    note: string;
+}
+/**
+ * nameOnCard
+ * cardNumber
+ * expirationDate
+ * cvc
+ * address
+ * apartment
+ * city
+ * region
+ * postalCode
+ * sameAsShipping
+ *
+ */
+
 const CheckoutForm = () => {
+    const handeleSubmit: FormEventHandler<HTMLFormElement> = (e) => {
+        e.preventDefault();
+        console.log(e.target);
+    };
+
+    const [firstName, setFirstName] = useState("");
+
+    const handleChangeEmailChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+        setFirstName(e.target.value);
+    };
+
     return (
         <div className="flex flex-col md:w-full">
             <h2 className="mb-4 font-bold md:text-xl text-heading ">Shipping Address</h2>
-            <form className="justify-center w-full mx-auto">
+            <form onSubmit={handeleSubmit} className="justify-center w-full mx-auto">
                 <div className="">
                     <div className="space-x-0 lg:flex lg:space-x-4">
                         <div className="w-full lg:w-1/2">
@@ -15,44 +51,49 @@ const CheckoutForm = () => {
                                 name="firstName"
                                 type="text"
                                 placeholder="First Name"
-                                className="w-full px-4 py-3 text-sm border border-gray-300 rounded lg:text-sm focus:outline-none focus:ring-1 focus:ring-blue-600"
+                                className="w-full px-4 py-3 text-sm border border-gray-300 rounded lg:text-sm focus:outline-none focus:ring-1 focus:ring-black"
+                                value={firstName}
+                                onChange={handleChangeEmailChange}
                             />
                         </div>
                         <div className="w-full lg:w-1/2 ">
-                            <label htmlFor="firstName" className="block mb-3 text-sm font-semibold text-gray-500">
+                            <label htmlFor="lastName" className="block mb-3 text-sm font-semibold text-gray-500">
                                 Last Name
                             </label>
                             <input
-                                name="Last Name"
+                                name="lastName"
                                 type="text"
                                 placeholder="Last Name"
-                                className="w-full px-4 py-3 text-sm border border-gray-300 rounded lg:text-sm focus:outline-none focus:ring-1 focus:ring-blue-600"
+                                className="w-full px-4 py-3 text-sm border border-gray-300 rounded lg:text-sm focus:outline-none focus:ring-1 focus:ring-black"
                             />
                         </div>
                     </div>
                     <div className="mt-4">
                         <div className="w-full">
-                            <label htmlFor="Email" className="block mb-3 text-sm font-semibold text-gray-500">
+                            <label htmlFor="emailAdress" className="block mb-3 text-sm font-semibold text-gray-500">
                                 Email
                             </label>
                             <input
-                                name="Last Name"
-                                type="text"
+                                name="emailAdress"
+                                type="email"
                                 placeholder="Email"
-                                className="w-full px-4 py-3 text-sm border border-gray-300 rounded lg:text-sm focus:outline-none focus:ring-1 focus:ring-blue-600"
+                                id="email=adress"
+                                autoComplete="email"
+                                className="w-full px-4 py-3 text-sm border border-gray-300 rounded lg:text-sm focus:outline-none focus:ring-1 focus:ring-black"
                             />
                         </div>
                     </div>
                     <div className="mt-4">
                         <div className="w-full">
-                            <label htmlFor="Address" className="block mb-3 text-sm font-semibold text-gray-500">
+                            <label htmlFor="address" className="block mb-3 text-sm font-semibold text-gray-500">
                                 Address
                             </label>
-                            <textarea
-                                className="w-full px-4 py-3 text-xs border border-gray-300 rounded lg:text-sm focus:outline-none focus:ring-1 focus:ring-blue-600"
-                                name="Address"
+                            <input
+                                type="text"
+                                className="w-full px-4 mb-3 py-3 text-sm border border-gray-300 rounded lg:text-sm focus:outline-none focus:ring-1 focus:ring-black"
+                                name="address"
                                 placeholder="Address"
-                            ></textarea>
+                            ></input>
                         </div>
                     </div>
                     <div className="space-x-0 lg:flex lg:space-x-4">
@@ -64,18 +105,18 @@ const CheckoutForm = () => {
                                 name="city"
                                 type="text"
                                 placeholder="City"
-                                className="w-full px-4 py-3 text-sm border border-gray-300 rounded lg:text-sm focus:outline-none focus:ring-1 focus:ring-blue-600"
+                                className="w-full px-4 py-3 text-sm border border-gray-300 rounded lg:text-sm focus:outline-none focus:ring-1 focus:ring-black"
                             />
                         </div>
                         <div className="w-full lg:w-1/2 ">
-                            <label htmlFor="postcode" className="block mb-3 text-sm font-semibold text-gray-500">
+                            <label htmlFor="postalCode" className="block mb-3 text-sm font-semibold text-gray-500">
                                 Postcode
                             </label>
                             <input
-                                name="postcode"
+                                name="postalCode"
                                 type="text"
                                 placeholder="Post Code"
-                                className="w-full px-4 py-3 text-sm border border-gray-300 rounded lg:text-sm focus:outline-none focus:ring-1 focus:ring-blue-600"
+                                className="w-full px-4 py-3 text-sm border border-gray-300 rounded lg:text-sm focus:outline-none focus:ring-1 focus:ring-black"
                             />
                         </div>
                     </div>
@@ -90,12 +131,11 @@ const CheckoutForm = () => {
                     </div>
                     <div className="relative pt-3 xl:pt-6">
                         <label htmlFor="note" className="block mb-3 text-sm font-semibold text-gray-500">
-                            {" "}
                             Notes (Optional)
                         </label>
                         <textarea
                             name="note"
-                            className="flex items-center w-full px-4 py-3 text-sm border border-gray-300 rounded focus:outline-none focus:ring-1 focus:ring-blue-600"
+                            className="flex items-center w-full px-4 py-3 text-sm border border-gray-300 rounded focus:outline-none focus:ring-1 focus:ring-black"
                             placeholder="Notes htmlFor delivery"
                         ></textarea>
                     </div>
