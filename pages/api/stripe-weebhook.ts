3ad1365361344deb578eae116936bd6d8547565f
GrzegorzNecka <global-strzypior@gmail.com>
2022-08-17 14:31:35 +0200

    try to use webhook

--- pages/api/stripe-weebhook.ts
+++ pages/api/stripe-weebhook.ts
@@ -1,11 +1,26 @@
 import { NextApiHandler } from "next/types";
+import { StripeWebhookEvents } from "stripeEvents";
 
 const stripeWebhook: NextApiHandler = (req, res) => {
     //@todo verify signing secret
 
-    console.log(req.body);
+    const event = req.body as StripeWebhookEvents;
+
+    switch (event.type) {
+        case "checkout.session.completed":
+            event.data.object.id;
+            // @todo zaktualizuj zamówienie w GraphCms
+            return;
+        case "checkout.session.async_payment_failed":
+            // @todo zaktualizuj zamówienie w GraphCms
+            return;
+        case "checkout.session.async_payment_succeeded":
+            // @todo zaktualizuj zamówienie w GraphCms
+            return;
+    }
 
     res.status(204).end();
 };
 
+// whsec_34d50c687371f7d7dba4023a90fe5a8d2997d2587bce1d8b8759a1765d472fde
 export default stripeWebhook;
