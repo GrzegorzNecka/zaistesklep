6c36e40b59455c5c4eba9825d9466f8f809b7d32
GrzegorzNecka <global-strzypior@gmail.com>
2022-08-23 08:39:48 +0200

    temporary fix line_items stripe object

--- pages/api/checkout.ts
+++ pages/api/checkout.ts
@@ -8,7 +8,6 @@ import { NextApiHandler } from "next/types";
 import Stripe from "stripe";
 
 const checkoutHandler: NextApiHandler = async (req, res) => {
-    console.log("🚀 ~ file: checkout.ts ~ line 5 ~ constcheckoutHandler:NextApiHandler= ~ req", req.body);
     const stripeKey = process.env.STRIPE_SECRET_KEY;
 
     if (!stripeKey) {
@@ -21,16 +20,12 @@ const checkoutHandler: NextApiHandler = async (req, res) => {
         count: number;
     }[];
 
-    console.log("🚀 ~ file: checkout.ts ~ line 20 ~ constcheckoutHandler:NextApiHandler= ~ body", body);
-
     const products = await Promise.all(
         body.map(async (cartItem) => {
             const product = await apolloClient.query<GetProductDetailsBySlugQuery, GetProductBySlugQueryVariables>({
                 query: GetProductBySlugDocument,
                 variables: { slug: cartItem.slug },
             });
-
-            // product.data.product
 
             return {
                 product,
@@ -39,9 +34,28 @@ const checkoutHandler: NextApiHandler = async (req, res) => {
         })
     );
 
-    // console
-
-    // const stripe = new Stripe(stripeKey, { apiVersion: "2020-08-27" });
+    const line_items = products.map((product) => {
+        return {
+            adjustable_quantity: {
+                enabled: true,
+                minimum: 0,
+                maximum: product.count,
+            },
+            price_data: {
+                currency: "PLN",
+                unit_amount: product.product.data.product!.price,
+                product_data: {
+                    name: product.product.data.product!.name,
+                    images: product.product.data.product!.images.map((i) => i.url),
+                    metadata: {
+                        slug: product.product.data.product!.slug,
+                        id: product.product.data.product!.id,
+                    },
+                },
+            },
+            quantity: product.count,
+        };
+    });
 
     const stripe = new Stripe(stripeKey, { apiVersion: "2022-08-01" });
 
@@ -50,32 +64,8 @@ const checkoutHandler: NextApiHandler = async (req, res) => {
         payment_method_types: ["p24", "card"],
         success_url: `${process.env.NEXT_PUBLIC_HOST}/checkout/success?session_id={CHECKOUT_SESSION_ID}`,
         cancel_url: `${process.env.NEXT_PUBLIC_HOST}/checkout/cancel`,
-        // line_items: JSON.parse(req.body),
-        line_items: products.map((product) => {
-            return {
-                adjustable_quantity: {
-                    enabled: true,
-                    minimum: 0,
-                    maximum: product.count,
-                },
-                price_data: {
-                    currency: "PLN",
-                    unit_amount: product.product.data.product?.price,
-                    product_data: {
-                        name: product.product.data.product?.name,
-                        images: product.product.data.product?.images.map((i) => i.url),
-                        metadata: {
-                            slug: product.product.data.product?.slug,
-                            id: product.product.data.product?.id,
-                        },
-                    },
-                },
-                quantity: product.count,
-            };
-        }),
-        mode: "payment",
-
-        // stworzenie order z tego miejsca w graphCMS
+        line_items: line_items,
+        // line_items: dev_line_Items,
     });
 
     res.status(201).json({ session: stripeCheckoutSession });
